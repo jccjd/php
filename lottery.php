24cec10e3324b661602ec9644c2d8bb813e3a1e0
jccjd <1340944538@qq.com>
2018-09-29 21:45:09 +0800

    lottery pro

--- lottery.php
+++ lottery.php
@@ -5,3 +5,25 @@
  * Date: 2018/9/29
  * Time: 21:34
  */
+$arr = array(
+    ["kk" => "001","ll" => "李四a"],
+    ["kk" => "002","ll" => "李四d"],
+    ["kk" => "003","ll" => "李四x"],
+    ["kk" => "004","ll" => "李四x"],
+    ["kk" => "005","ll" => "李四r"],
+    ["kk" => "006","ll" => "李四r"],
+    ["kk" => "007","ll" => "李四w"],
+    ["kk" => "008","ll" => "李w四"],
+
+);
+foreach ($arr as $v) {
+    echo "<p>";
+    echo "学号：".$v["kk"].",姓名：".$v['xm'];
+    echo "</p>";
+    if ($_GET['抽奖']='draw') {
+        $v = $arr[array_rand($arr,1)];
+        echo "<h2 style='margin-left: 400px;margin-top: -40% '>";
+        echo '恭喜学号为：'.$v["xh"].'，姓名为：'.$v["xm"].'的同学中奖';
+        echo "</h2>";
+    }
+}
