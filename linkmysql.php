53fb6caf89c623ab228546101d1b84ad2e56ac03
jccjd <1340944538@qq.com>
2018-10-09 21:54:59 +0800

    up

--- linkmysql.php
+++ linkmysql.php
@@ -17,3 +17,16 @@ $arr = array(1,2,3);
 echo "<pre>";
 print_r($arr);
 echo "</pre>";
+class a {
+    protected $c;
+    public function a() {
+        $this->c = 10;
+    }
+}
+class b extends a{
+    public function print_data() {
+        return $this->c;
+    }
+}
+$b = new b();
+echo $b->print_data();
