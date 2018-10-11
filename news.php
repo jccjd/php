626f4bfb85f49af473dcae44a7d978b4f19a6baf
jccjd <1340944538@qq.com>
2018-10-11 21:50:58 +0800

    up

--- news.php
+++ news.php
@@ -1,43 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <meta charset="utf-8">
-</head>
-<style type="text/css">
-    ul li {
-        list-style: none;
-        width: 500px;
-        height: 40px;
-        line-height: 20px;
-        border-bottom: 1px dotted #FFee33;
-    }
-    ul li a{
-        text-decoration: none;
-        color: red;
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/11
+ * Time: 19:44
+*/
 
-    }
-    li span{float: right;}
-</style>
-<body>
-<ul>
-    <?php
-    mysql_connect('127.0.0.1','root','root');
-    mysql_select_db('demo');
-    mysql_query("set names utf8");
+mysql_connect('127.0.0.1','root','root');
+mysql_select_db('demo');
+mysql_query("set names utf8");
 
-    $sql = "select * from newsInfo order by newsDateTime desc,id desc";
-    $res = mysql_query($sql);
-    while ($row = mysql_fetch_assoc($res)) {
-        # code...
-        ?>
-        <li>
-            <span><?php echo @date("Y-m-d",strtotime($rom['newsDateTime']))?></span>
-            <a href="news.php?id=<?php echo $row["id"] ?>"><?php echo $row["newsTitle"]?></a>
-        </li>
-    <?php }?>
-
-</ul>
-</body>
-
-</html>
+$sql = "select * from newsInfo order by newsDateTime desc,id desc";
+$res = mysql_query($sql);
+while ($row = mysql_fetch_assoc($res)) {
+    # code...
+        echo @date("Y-m-d",strtotime($rom['newsDateTime']));
+        echo $row["newsTitle"];
+}
