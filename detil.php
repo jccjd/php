4e412cbc9d0bb3c2977128cdb4f2b284e9c6684d
jccjd <1340944538@qq.com>
2018-10-14 17:16:52 +0800

    up

--- detil.php
+++ detil.php
@@ -25,17 +25,19 @@
  * Time: 22:20
  */
 $id = $_GET['id'];
+//链接数据库
 mysql_connect('127.0.0.10','root','root');
 mysql_select_db('demo');
 mysql_query('set names utf8');
 
-
+//更改数据
 $sql="update newsInfo set newsCnt=newsCnt+1 where id=$id";
 $res=mysql_query($sql);
-
+//、、查询数据
 $sql="select * from newsInfo where id=$id";
 $res=mysql_query($sql);
 $row=mysql_fetch_assoc($res);
+
 echo "<h1>".$row['newsTitle']."</h1>";
 echo "<p><span>".$row['newsAuthor']."</span>"."<span class='little'>时间".@date("Y-m-d",strtotime($row['newsDateTime']))."</span>".
     "<span class='little'>浏览量：".$row['newsCnt']."</span><br>";
