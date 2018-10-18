eb55fd8ddd91e27b964efcd0a128dd4859a0ee74
jccjd <1340944538@qq.com>
2018-10-18 19:23:02 +0800

    up

--- news.php
+++ news.php
@@ -27,7 +27,7 @@ mysql_connect('127.0.0.1','root','root');
 mysql_select_db('demo');
 mysql_query("set names utf8");
 
-$sql = "select * from newsInfo order by newsDateTime desc,id desc";
+$sql = "select * from newsInfo order by newsDateTime desc";
 $res = mysql_query($sql);
 while ($row = mysql_fetch_assoc($res)) {
     # code...
