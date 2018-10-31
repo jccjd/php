d84eb4d4d29a7163186931e2510dab145649277d
jccjd <1340944538@qq.com>
2018-10-31 21:22:10 +0800

    up

---
+++ testCopy/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/31
+ * Time: 21:18
+ */
+class People {
+
+    protected $_name = '张三';
+    protected $_sex = '男';
+    protected $_age = '18';
+
+}
