50021a126aa43c2899b8a3b44d0ad9895d4a2fe0
jccjd <1340944538@qq.com>
2018-10-31 21:18:00 +0800

    up

--- news.php
+++ news.php
@@ -56,7 +56,6 @@ while ($row = mysql_fetch_assoc($res)) {
     echo "<ul><li>";
     echo "<span>".@date("Y-m-d",strtotime($rom['newsDateTime']))."</span>";
     echo "<a href ='detil.php?id={$row['id']}'>".$row["newsTitle"]."</a>";
-
     echo "</li></ul>";
 }
 //上页下页
