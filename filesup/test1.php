668ff6fa3f98972b22746a4118c76a4a13dbbf7f
jccjd <1340944538@qq.com>
2018-11-14 20:08:44 +0800

    up

--- filesup/test1.php
+++ filesup/test1.php
@@ -1,3 +1,6 @@
+<html>
+<meta charset="UTF-8">
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +8,11 @@
  * Date: 2018/11/14
  * Time: 19:29
  */
+if ($_FILES['file']['error'] > 0) {
+    echo "错误：".$_FILES['file']['error'];
+} else {
+    echo '文件名：'.$_FILES['file']['name']."<br>";
+    echo '文件类型：'.$_FILES['file']['type']."<br>";
+    echo '文件大小：'.$_FILES['file']['size']."<br>";
+    echo '文件存储位置：'.$_FILES['file']['tmp_name'];
+}
