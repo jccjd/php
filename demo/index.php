60ec9d1f2f126465f6685ea4f70ab03e9ad78012
jccjd <1340944538@qq.com>
2018-11-19 22:07:32 +0800

    up

--- demo/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$control=$_GET["controlName"];
-require $control.'control.class.php';
-$user=new $control;
-$cfunction=$_GET["controlFunction"];
-$user->$cfunction();
--- demo/msyql.php
+++
@@ -1,4 +0,0 @@
-<?php
-require "MySQLPDO.class.php";
-$arr=array("pass"=>"root","dbname"=>"demo");
-$mysql=MySQLPDO::getInstance($arr);
