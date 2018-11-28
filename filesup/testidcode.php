f037c5ee8b646adadeab881be3878e35f50c2d3c
jccjd <1340944538@qq.com>
2018-11-28 22:49:11 +0800

    idcode

--- filesup/testidcode.php
+++ filesup/testidcode.php
@@ -9,11 +9,9 @@
 //要用到的字符
 $code = array_merge(range('A','Z'),range('a','z'),range(1,9));
 $keyCode = array_rand($code, 4);
-print_r($keyCode);
 shuffle($keyCode);
-
 $verifyCode = "";
 foreach ($keyCode as $key) {
+    /*得到真的验证码值*/
     $verifyCode .= $code[$key];
 }
-print_r($verifyCode);
