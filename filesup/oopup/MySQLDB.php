14aaaddad1c2d6d36d3062be3b3d7c485d365a4e
jccjd <1340944538@qq.com>
2018-12-25 20:27:22 +0800

    oopup

--- filesup/oopup/MySQLDB.php
+++ filesup/oopup/MySQLDB.php
@@ -8,7 +8,7 @@
 
 class MySQLDB {
     //数据库连接信息
-    private  $dbCOnfig = array(
+    private  $dbConfig = array(
         'host' => 'localhost', //主机
         'port' => '3306',//端口
         'user' => '',//用户
@@ -21,8 +21,59 @@ class MySQLDB {
     private $link;
     //单例对象
     private static $instance;
-    /**/
+    /**
+     * 获得单例对象的公共接口方法
+     * @param array $params 数据库连接信息
+     * @return object 单例对象
+     * */
     public static function getInstance($params = array()) {
+        //判断有没有被实例化
+        if (!self::$instance instanceof self) {
+            //实例化并保存
+            self::$instance = new self($params);
+        }
+        return self::$instance;
+    }
+
+    /**
+     * 构造方法
+     * @param array  @params 关联数组
+     */
+
+    public function __construct($params = array())
+    {
+        //初始化数据库连接信息
+        $this->inintAttr($params);
+        //连接数据库
+        $this->connectServer();
+        //设置字符集
+        $this->setCharset();
+        //选择默认数据库
+        $this->selectDefaultDb();
+    }
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+    /**
+     * 私有化克隆
+     */
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+    private function initArrt($params) {
+        //初始化属性， 使用array_marge() 函数合并两个数组
+        $this->dbConfig = array_merge($this->dbConfig,$params);
+    }
+    /**
+     * 连接目标服务器
+     * */
+    private function connectServer() {
+        $host = $this->dbConfig['host'];
+        $_port = $this->dbConfig['port'];
+        $user = $this->dbConfig['user'];
+        $pwd = $this->dbConfig['pwd'];
 
     }
 }
