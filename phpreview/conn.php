81901d7303e27331225db10bdfac082cf9716196
jccjd <1340944538@qq.com>
2018-12-28 20:28:50 +0800

    review php

---
+++ phpreview/conn.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/12/28
+ * Time: 20:27
+ */
+mysql_connect('127.0.0.1','root','root');
+mysql_select_db('demo');
+mysql_query('set name utf8');
