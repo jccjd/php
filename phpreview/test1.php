aaa5be08a63cc15acb414f99393859bb9c65759e
jccjd <1340944538@qq.com>
2018-12-29 22:38:24 +0800

    review php

--- phpreview/test1.php
+++ phpreview/test1.php
@@ -1,3 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Til</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+        p{
+            text-align: center;
+        }
+    </style>
+</head>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -6,4 +20,5 @@
  * Time: 20:19
  */
 /*连接数据库*/
+require 'conn.php';
 
