79b67a16b24951a901106678e7896e6efba69e1b
jccjd <1340944538@qq.com>
2019-01-02 21:01:35 +0800

    review php

--- phpreview/index.php
+++ phpreview/index.php
@@ -1,3 +1,34 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>主页</title>
+</head>
+<style>
+    li {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    a:link {
+        color: antiquewhite;
+    }
+
+    li span {
+        float: right;
+    }
+
+    .a {
+        float: right;
+        padding-right: 10px;
+    }
+</style>
+<body>
+
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +36,14 @@
  * Date: 2019/1/2
  * Time: 12:32
  */
+require 'conn.php';
+$sql = 'select * from newsInfo';
+$res = mysql_query($sql);
+while ($eow = mysql_fetch_assoc($res)) {
+    echo '<ul><li>';
+    echo "<a href='news.php?id={$row['id']}'>".$row['newsTitle']."</a>";
+    echo "<span>".date('y-m-d',strtotime($row['newsDateTime']))."</span>";
+    echo "<a href='test1.php?id{$row['id']}' class='a'>".删除."</a>";
+    echo "<a href='test1.php?id{$row['id']}' class='a'>".修改."</a>";
+    echo '</li></ul>';
+}
