74dadda29c48b1cc7026e1b798fec41ca069a2d2
jccjd <1340944538@qq.com>
2019-01-04 16:43:58 +0800

    review php

--- filesup/singin.php
+++ filesup/singin.php
@@ -21,7 +21,6 @@
         if (mysql_fetch_assoc($res)) {
             die("用户名已存在，请重新输入");
         } else {
-            // $data["userPwd"]=md5($data["userPwd"]);
             $salt = rand(1000, 9999);
             $data["salt"] = $salt;
             $data["userPwd"] = md5(md5($data['userPwd']) . $salt);
@@ -32,7 +31,6 @@
             }
             $sql =$sql."image='"."$newname'";
             $sql = rtrim($sql, ',');
-            echo $sql;
             $res = mysql_query($sql);
             if ($res) {
                 echo "<script>alert('注册成功')</script>";
