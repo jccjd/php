4bd19818b3ccb698053d0cf804dd46083b259c67
jccjd <1340944538@qq.com>
2019-01-05 15:36:09 +0800

    review

--- phpreview/upfiles/up.php
+++ phpreview/upfiles/up.php
@@ -2,6 +2,51 @@
 /**
  * Created by PhpStorm.
  * User: lenovo
- * Date: 2019/1/5
- * Time: 14:07
+ * Date: 2018/11/14
+ * Time: 19:29
+ * 上传图片将其保存到本地的upload/里
+ * 并生成的新名字交由imgwater.php使用
  */
+
+/*保存路径*/
+$path = "upload/";
+//允许上传的图片的后缀
+$allowedExts = array('gif','jpeg','jpg','png');
+
+
+$file = $_FILES['file'];
+$filename = $file['tmp_name'];
+$image_size = getimagesize($filename);
+
+//获取文件后缀
+$temp = explode('.',$file['name']);
+$extension = end($temp);
+
+/*判断文件大小和类型是否匹配*/
+if ( ($file['size'] < 204800) && in_array($extension,$allowedExts)) {
+
+    if ($file['error'] > 0) {
+
+        echo "错误：".$file['error'];
+
+    } else {
+
+
+        /*判断当前目录下的upload 目录是否存在该文件
+          如果没有该目录，创建之
+        */
+        if (!file_exists($path)) {
+            mkdir($path);
+        } else {
+            //如果upload 目录下没有就可以上传到这里了
+//            move_uploaded_file($_FILES['file']['tmp_name'],"upload/".$_FILES['file']['name']);
+//            echo '文件地址：'.'upload/'.$_FILES['file']['name'];
+            $newname = date("YmdHis").rand(1000,9000).".jpg";
+            $newfilename = 'upload/'.$newname;
+            move_uploaded_file($_FILES['file']['tmp_name'],$newfilename);
+        }
+    }
+} else {
+    echo "非法格式";
+}
+
