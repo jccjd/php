e088df8cf9e5190aed5023bcf7a3e9156be8e807
jccjd <1340944538@qq.com>
2019-01-06 22:01:53 +0800

    review

--- phpreview/upfiles/water.php
+++ phpreview/upfiles/water.php
@@ -7,4 +7,25 @@
  * 图片加水印
  */
 require 'up.php';
+$watering = 'hell0';
 $innfo = getimagesize($path.$newname,$innfo);
+$nimage = imagecreatetruecolor($image_size[0],$image_size[1]);
+$white = imagecolorallocate($nimage,255,255,255);
+$black = imagecolorallocate($nimage,0,0,0);
+
+imagefill($nimage,0,0,$white);
+switch ($innfo[2]) {
+    case 1:
+        $simage = imagecreatefromgif($path.$newname);
+    case 2:
+        $simage = imagecreatefromjpg($path.$newname);
+    case 3:
+        $simage = imagecreatefrompng($path.$newname);
+        break;
+    default:
+        die("不支持的文件类型");
+        exit;
+}
+imagecopy($nimage,$simage,0,0,0,0,$image_size[0],$image_size[1]);
+imagefilledrectangle($nimage,1,$image_size[1]-15,80,$image_size[1],$white);
+imagestring($nimage,2,3,$image_size[1]-15,$watering,$black);
